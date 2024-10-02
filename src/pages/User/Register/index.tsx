1c76eb56074085e00b71300ae8231830bb5e5016
BrenoJLeal02 <copywriting.leal@gmail.com>
2024-10-02 18:04:13 -0300

    feat: adicionados os esqueletos do Login, Registro e Forgot-Password

--- src/pages/User/Register/index.tsx
+++ src/pages/User/Register/index.tsx
@@ -1,7 +1,102 @@
-export function Register(){
+import { useState, FormEvent } from 'react';
+import { Button, Input, FormControl, FormLabel, Box, Text, Link,  } from '@chakra-ui/react';
+import { signUp } from '../../../service/Auth';
 
-    return(
-        <>
-        </>
-    )
+export function Register() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>('')
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+   const handleSubmit = async (e: FormEvent) => {
+     e.preventDefault();
+     setLoading(true);
+     setError(null);
+     try {
+       const user = await signUp({ username, password, email, confirmPassword });
+       console.log('Usuário autenticado:', user);
+     } catch (error: unknown) {
+       if (error instanceof Error) {
+         setError(error.message);
+       } else {
+         setError('Ocorreu um erro inesperado!');
+       }
+     } finally {
+       setLoading(false);
+     }
+   };
+
+  return (
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+    >
+      <Box width="300px">
+        <form onSubmit={handleSubmit}>
+        <FormControl mb="4">
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              id="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              isDisabled={loading}
+            />
+          </FormControl>
+
+          <FormControl mb="4">
+            <FormLabel htmlFor="username">Username</FormLabel>
+            <Input
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              isDisabled={loading}
+            />
+          </FormControl>
+
+          <FormControl mb="4">
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              isDisabled={loading}
+            />
+          </FormControl>
+          <FormControl mb="4">
+            <FormLabel htmlFor="confirmPassword">Confirm Password</FormLabel>
+            <Input
+              id="confirmPassword"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              isDisabled={loading}
+            />
+          </FormControl>
+          {error && (
+            <Text color="red.500" mb="4">
+              {error}
+            </Text>
+          )}
+        
+          <Button
+            type="submit"
+            colorScheme="brand"
+            width="100%"
+            isLoading={loading}
+            isDisabled={loading}
+          >
+            Registrar
+          </Button>
+          <Box>Já possui uma conta? <Link href='/' color='blue.500'>Login</Link></Box>
+        </form>
+      </Box>
+    </Box>
+  );
 }
