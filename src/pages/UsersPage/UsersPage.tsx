615f67d57b237d50332b86c7c927d63ffbb0d20d
Clara Ferraz <claraferraz@proton.me>
2024-10-22 18:12:59 -0300

    Styles: responsividade, barra de pesquisa e título

--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -1,5 +1,14 @@
+import {
+  Grid,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Tab,
+  Tabs,
+} from '@chakra-ui/react';
 import { UserCard } from '../../components/UserCard/UserCard';
 import { UserCardInfos } from '../../interface/UserInterface';
+import { FiSearch } from 'react-icons/fi';
 
 export function UsersPage() {
   const users: UserCardInfos[] = [
@@ -30,14 +39,41 @@ export function UsersPage() {
   ];
   return (
     <>
-      {users.map((u: UserCardInfos) => (
-        <UserCard
-          name={u.name}
-          username={u.username}
-          posts={u.posts}
-          id={u.id}
-        />
-      ))}
+      <Tabs width="100%">
+        <Tab width="100%" color="#281A45">
+          Usuários
+        </Tab>
+      </Tabs>
+
+      <InputGroup
+        margin="30px 0"
+        borderRadius={6}
+        width={{ base: '100%', md: '350px' }}
+      >
+        <InputLeftElement children={<FiSearch color="gray.300" />} />
+        <Input placeholder="Pesquisar usuário" />
+      </InputGroup>
+
+      <Grid
+        justifyItems="center"
+        templateColumns={{
+          base: 'repeat(1, 1fr)',
+          sm: 'repeat(2, 1fr)',
+          md: 'repeat(3, 1fr)',
+          lg: 'repeat(4, 1fr)',
+        }}
+        gap="15px"
+        overflow="hidden"
+      >
+        {users.map((u: UserCardInfos) => (
+          <UserCard
+            name={u.name}
+            username={u.username}
+            posts={u.posts}
+            id={u.id}
+          />
+        ))}
+      </Grid>
     </>
   );
 }
