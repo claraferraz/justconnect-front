432f2e1f35441d589ba1fc33abee1f49fd1060e7
Clara Ferraz <claraferraz@proton.me>
2024-10-22 17:00:54 -0300

    Feat: adicao do user card no users page

--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -1,7 +1,43 @@
+import { UserCard } from '../../components/UserCard/UserCard';
+import { UserCardInfos } from '../../interface/UserInterface';
+
 export function UsersPage() {
+  const users: UserCardInfos[] = [
+    {
+      name: 'Clara Ferraz',
+      username: 'claraferraz',
+      posts: 10,
+      id: '9680e272-5c8b-457a-b716-daadcba84e8c',
+    },
+    {
+      name: 'Breno Leal',
+      username: 'brenoleal',
+      posts: 15,
+      id: 'e2085ca8-ae0f-4152-ae8e-9b75cb5ae486',
+    },
+    {
+      name: 'Kaio Paulo',
+      username: 'kaiopaulo',
+      posts: 15,
+      id: '0637ca29-e2c2-4190-b9be-40a62b5f7068',
+    },
+    {
+      name: 'Gustavo Santos',
+      username: 'gustavosantos',
+      posts: 15,
+      id: '4454bb54-225d-4898-a8f5-1cf6688ad40b',
+    },
+  ];
   return (
     <>
-      <h1>Users List Page</h1>
+      {users.map((u: UserCardInfos) => (
+        <UserCard
+          name={u.name}
+          username={u.username}
+          posts={u.posts}
+          id={u.id}
+        />
+      ))}
     </>
   );
 }
