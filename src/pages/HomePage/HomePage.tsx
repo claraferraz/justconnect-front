56c919b90bf80945d31ceabfcf2ff8018a53d1d6
Gustavo Santos <gustavogomes.bsb68@gmail.com>
2024-10-21 21:02:21 -0300

    cards

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,36 +1,112 @@
-import { Avatar, Box, Heading, Input, Stack } from "@chakra-ui/react";
+import { Avatar, 
+  Box, 
+  Button, 
+  Card, 
+  CardBody, 
+  CardFooter, 
+  CardHeader, 
+  Flex, 
+  Heading, 
+  Input, 
+  Stack, 
+  Tab, 
+  TabList, 
+  TabPanel, 
+  TabPanels, 
+  Tabs } from "@chakra-ui/react";
 import { SearchIcon, BellIcon } from "@chakra-ui/icons";
 
 export function HomePage() {
   return (
     <>
-      <Box 
-        display="flex" 
-        alignItems="center" 
-        justifyContent="space-between" 
-        margin="25px" 
-        flexWrap="wrap"
+      <header >
+        <Box 
+          display="flex" 
+          alignItems="center" 
+          justifyContent="space-between" 
+          margin="25px" 
+          flexWrap="wrap"
+        >
+          {/* BellIcon e Avatar alinhados à esquerda */}
+          <Box display="flex" alignItems="center">
+            <Heading marginLeft="auto">Just Connect</Heading>
+          </Box>
+
+          {/* SearchIcon e Input centralizados */}
+          <Box display="flex" alignItems="center" flexGrow={1} justifyContent="center">
+            <SearchIcon boxSize={6} marginRight="10px" />
+            <Input width={{ base: "200px", md: "300px" }} placeholder='Pesquisar' />
+          </Box>
+
+          {/* Heading alinhado à direita */}
+          <BellIcon boxSize={8} marginRight="5px" />
+            <Stack>
+              <Avatar src='https://bit.ly/broken-link'/>
+            </Stack>
+        </Box>
+      </header>
+
+      <Box
+      display="flex"
+      alignItems=""
+      justifyContent="center"
+      height="100vh"
+      margin="30px"
+      color="purple"
       >
-        {/* BellIcon e Avatar alinhados à esquerda */}
-        <Box display="flex" alignItems="center">
-          <Heading marginLeft="auto">Just Connect</Heading>
-        </Box>
+        <Tabs isFitted variant='enclosed'>
+          <TabList mb='1em'>
+            <Tab>Recentes</Tab>
+            <Tab>Relevantes</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Card maxW='md'>
+                <CardHeader>
+                  <Flex>
+                    <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                      <Avatar name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />
 
-        {/* SearchIcon e Input centralizados */}
-        <Box display="flex" alignItems="center" flexGrow={1} justifyContent="center">
-          <SearchIcon boxSize={6} marginRight="10px" />
-          <Input width={{ base: "200px", md: "300px" }} placeholder='Pesquisar' />
-        </Box>
-
-        {/* Heading alinhado à direita */}
-        <BellIcon boxSize={8} marginRight="5px" />
-          <Stack>
-            <Avatar src='https://bit.ly/broken-link'/>
-          </Stack>
-      </Box>
-
-      <Box>
-        {/* Outros conteúdos da página */}
+                      <Box>
+                        <Heading size='sm'>Segun Adebayo</Heading>
+                        <p>Creator, Chakra UI</p>
+                      </Box>
+                    </Flex>
+                  </Flex>
+                </CardHeader>
+                <CardBody>
+                  <p>
+                    With Chakra UI, I wanted to sync the speed of development with the speed
+                    of design. I wanted the developer to be just as excited as the designer to
+                    create a screen.
+                  </p>
+                </CardBody>
+                <CardFooter
+                  justify='space-between'
+                  flexWrap='wrap'
+                  sx={{
+                    '& > button': {
+                      minW: '136px',
+                    },
+                  }}
+                >
+                  <Button flex='1' variant='ghost'>
+                    Like
+                  </Button>
+                  <Button flex='1' variant='ghost' >
+                    Comment
+                  </Button>
+                  <Button flex='1' variant='ghost'>
+                    Share
+                  </Button>
+                </CardFooter>
+              </Card>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       </Box>
     </>
   );
