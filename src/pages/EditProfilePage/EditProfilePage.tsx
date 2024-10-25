ed5e58ffd23be8d0b50e0d89314c56a450405e8e
Clara Ferraz <claraferraz@proton.me>
2024-10-25 18:53:30 -0300

    Styles: posição do excluir perfil e height da bio

--- src/pages/EditProfilePage/EditProfilePage.tsx
+++ src/pages/EditProfilePage/EditProfilePage.tsx
@@ -17,16 +17,16 @@ export function EditProfilePage() {
         <Flex direction="column" gap="50px" align="center">
           <Avatar name={user.name} fontWeight={800} size="lg" />
           <Link _hover={{ color: '#805AD5' }}>Alterar Senha</Link>
-
-          <Flex color="#D20000" _hover={{ color: '#9c0303' }}>
-            <Icon aria-label="editar" cursor="pointer" fontSize="24px">
-              <MdDeleteOutline />
-            </Icon>
-            <Link href="">Excluir Conta</Link>
-          </Flex>
         </Flex>
 
         <EditProfileForm user={user} />
+      </Flex>
+
+      <Flex margin="60px 0" color="#D20000" _hover={{ color: '#9c0303' }}>
+        <Icon aria-label="editar" cursor="pointer" fontSize="24px">
+          <MdDeleteOutline />
+        </Icon>
+        <Link href="">Excluir Conta</Link>
       </Flex>
     </>
   );
