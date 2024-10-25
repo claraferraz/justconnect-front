a5d2bf703692aa34cb6170c69533ac438719626d
Clara Ferraz <claraferraz@proton.me>
2024-10-25 20:57:37 -0300

    Feat and Refactor: funcao de login no authStore

---
+++ src/store/authStore.ts
@@ -0,0 +1,38 @@
+import { UUID } from 'crypto';
+import { create, StateCreator } from 'zustand';
+import { signIn } from '../service/Auth';
+import { devtools, persist } from 'zustand/middleware';
+
+export interface AuthState {
+  token?: string;
+  id?: UUID;
+
+  loginUser: (usernameOrEmail: string, password: string) => Promise<void>;
+}
+const storeApi: StateCreator<AuthState> = (set) => ({
+  token: undefined,
+  id: undefined,
+
+  loginUser: async (usernameOrEmail: string, password: string) => {
+    const isEmail = /\S+@\S+\.\S+/.test(usernameOrEmail);
+
+    try {
+      const user = await signIn({
+        username: isEmail ? '' : usernameOrEmail,
+        email: isEmail ? usernameOrEmail : '',
+        password,
+      });
+      const { token, id } = user.data;
+      set({ token, id });
+    } catch (error) {
+      console.error(error);
+    }
+  },
+
+  logoutUser: () => {
+    set({ token: undefined, id: undefined });
+  },
+});
+export const useAuthStore = create<AuthState>()(
+  devtools(persist(storeApi, { name: 'auth-storage' }))
+);
