a77d43f2b54517bd1705cb04b2a7dec18ef5fde4
Clara Ferraz <claraferraz@proton.me>
2024-10-25 21:13:55 -0300

    Feat and Refactor: dados de login no header a partir do store

--- src/service/Auth.ts
+++ src/service/Auth.ts
@@ -1,36 +1,34 @@
-import { UserForgot, UserSignIn, UserSingUp, UserResponse } from "../interface/UserInterface";
-import { apiAuth } from "./api";
+import { UUID } from 'crypto';
+import {
+  UserForgot,
+  UserSignIn,
+  UserSingUp,
+  UserResponse,
+} from '../interface/UserInterface';
+import { apiAuth } from './api';
 
 const signUp = async (data: UserSingUp) => {
-    const response = await apiAuth.post(`/public/users/register`, data);
-    return response;
+  const response = await apiAuth.post(`/public/users/register`, data);
+  return response;
 };
 
 const signIn = async (data: UserSignIn) => {
-    const response = await apiAuth.post(`/public/auth`, data);
-    return response;
+  const response = await apiAuth.post(`/public/auth`, data);
+  return response;
 };
 
 const forgot = async (data: UserForgot) => {
-    const response = await apiAuth.post(`/forgot`, data);
-    return response;
+  const response = await apiAuth.post(`/forgot`, data);
+  return response;
 };
 
-const fetchUserData = async (): Promise<UserResponse> => {
-    const id = localStorage.getItem('id');
+const fetchUserData = async (id?: UUID): Promise<UserResponse> => {
+  if (!id) {
+    throw new Error('ID não foi encontrado');
+  }
+  const response = await apiAuth.get<UserResponse>(`/public/users/${id}`);
 
-    if (!id) {
-        throw new Error('ID não foi encontrado'); 
-    }
-    const response = await apiAuth.get<UserResponse>(`/public/users/${id}`);
-    
-    return response.data; 
+  return response.data;
 };
 
-
-export {
-    signUp,
-    signIn,
-    forgot,
-    fetchUserData
-};
+export { signUp, signIn, forgot, fetchUserData };
