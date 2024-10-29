1909c3391a5cb045d0a700cdd692b279f1432720
BrenoJLeal02 <copywriting.leal@gmail.com>
2024-10-29 16:03:34 -0300

    fix: catch do authStore

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -25,8 +25,14 @@ const storeApi: StateCreator<AuthState> = (set) => ({
       });
       const { token, id } = user.data;
       set({ token, id });
-    } catch (error) {
-      console.error(error);
+    }  catch (error) {
+      if (error instanceof Error) {
+        console.error('Erro ao fazer login:', error.message);
+        throw new Error(error.message); 
+      } else {
+        console.error('Erro inesperado ao fazer login:', error);
+        throw new Error('Erro inesperado ao fazer login');
+      }
     }
   },
 
