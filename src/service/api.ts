006ae1834446dcdce23928e88387941fd6c44fe7
Breno Leal <brenoooleal@gmail.com>
2024-10-30 11:19:30 -0300

    refactor: remoção do código do api.ts que utilizava localstorage

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,14 +1,3 @@
-// import axios from 'axios';
-
-// const token = localStorage.getItem('token') || 'no token';
-
-// export const apiAuth = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-//   headers: {
-//     Authorization: `Bearer ${token}`,
-//   },
-// });
-
 import axios from 'axios';
 import { useAuthStore } from '../store/authStore'; 
 
