972d9474af2646ede3f3b8a2bd518e976a3a9bac
Clara Ferraz <claraferraz@proton.me>
2024-10-30 14:52:30 -0300

    Refactor and Feat: separação de service Auth e Profile, alteração do nome apiAuth para api e método de deleteProfile

--- src/service/Auth.ts
+++ src/service/Auth.ts
@@ -1,40 +1,19 @@
-import { UUID } from 'crypto';
-import {
-  UserForgot,
-  UserSignIn,
-  UserSingUp,
-  UserResponse,
-  MyProfileInfos,
-} from '../interface/UserInterface';
-import apiAuth from '../service/api';
+import { UserForgot, UserSignIn, UserSingUp } from '../interface/UserInterface';
+import api from '../service/api';
 
 const signUp = async (data: UserSingUp) => {
-  const response = await apiAuth.post(`/public/users/register`, data);
+  const response = await api.post(`/public/users/register`, data);
   return response;
 };
 
 const signIn = async (data: UserSignIn) => {
-  const response = await apiAuth.post(`/public/auth`, data);
+  const response = await api.post(`/public/auth`, data);
   return response;
 };
 
 const forgot = async (data: UserForgot) => {
-  const response = await apiAuth.post(`/forgot`, data);
+  const response = await api.post(`/forgot`, data);
   return response;
 };
 
-const fetchUserData = async (id?: UUID): Promise<UserResponse> => {
-  if (!id) {
-    throw new Error('ID não foi encontrado');
-  }
-  const response = await apiAuth.get<UserResponse>(`/public/users/${id}`);
-
-  return response.data;
-};
-
-const fetchMyProfile = async (): Promise<MyProfileInfos> => {
-  const response = await apiAuth.get<MyProfileInfos>(`/users/my-profile`);
-  return response.data;
-};
-
-export { signUp, signIn, forgot, fetchUserData, fetchMyProfile };
+export { signUp, signIn, forgot };
--- src/service/Profile.tsx
+++ src/service/Profile.tsx
@@ -1,48 +1,32 @@
 import { UUID } from 'crypto';
 import { MyProfileInfos, UserResponse } from '../interface/UserInterface';
-import { apiAuth } from './api';
-import axios from 'axios';
+import api from './api';
 
 const fetchUserData = async (id?: UUID): Promise<UserResponse> => {
   if (!id) {
     throw new Error('ID não foi encontrado');
   }
-  const response = await apiAuth.get<UserResponse>(`/public/users/${id}`);
+  const response = await api.get<UserResponse>(`/public/users/${id}`);
 
   return response.data;
 };
 
-const fetchMyProfile = async (token?: string): Promise<MyProfileInfos> => {
-  if (!token) {
-    throw new Error('Usuário deve estar logado');
-  }
-
-  const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
+const fetchMyProfile = async (): Promise<MyProfileInfos> => {
   const response = await api.get<MyProfileInfos>(`/users/my-profile`);
-
   return response.data;
 };
 
 const alterProfile = async (
-  token: string,
   id: UUID,
   data: MyProfileInfos
 ): Promise<MyProfileInfos> => {
-  const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
   const response = await api.put<MyProfileInfos>(`/users/${id}`, data);
 
   return response.data;
 };
 
-export { fetchUserData, fetchMyProfile, alterProfile };
+const deleteProfile = async (id: UUID) => {
+  await api.delete<MyProfileInfos>(`/users/${id}`);
+};
+
+export { fetchUserData, fetchMyProfile, alterProfile, deleteProfile };
