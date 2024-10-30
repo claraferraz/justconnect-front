64b70b2254d5754e8062d6e4c8464a45da67bd1c
Clara Ferraz <claraferraz@proton.me>
2024-10-30 15:47:46 -0300

    Styles: responsividade myProfile

--- src/pages/MyProfilePage/MyProfilePage.tsx
+++ src/pages/MyProfilePage/MyProfilePage.tsx
@@ -1,39 +1,48 @@
-import { Box, Icon, Link } from '@chakra-ui/react';
+import { Box, Flex, Icon, Link, useBreakpointValue } from '@chakra-ui/react';
 import { UserProfile } from '../../components/UserProfile/UserProfile';
 import { MdEdit } from 'react-icons/md';
 import { useProfileStore } from '../../store/profileStore';
 
 export function MyProfilePage() {
+  const isDesktop = useBreakpointValue({ base: false, md: true });
   const user = useProfileStore((state) => state.user);
   if (!user) {
     return;
   }
   return (
     <>
-      <Box textAlign="right">
-        <Link href="/my-profile/edit">
-          <Icon
-            mt="15px"
-            aria-label="editar"
-            cursor="pointer"
-            fontSize="24px"
-            color="#281A45"
-            _hover={{ color: '#805AD5' }}
-          >
-            <MdEdit />
-          </Icon>
-        </Link>
-      </Box>
-
-      <UserProfile
-        name={user.name}
-        username={user.username}
-        bio={user.bio}
-        insta={user.insta}
-        linkedin={user.linkedin}
-        github={user.github}
-        adminBlock={false}
-      />
+      <Flex
+        direction="column"
+        justify="center"
+        margin="0 auto"
+        maxWidth={isDesktop ? '1024px' : '350px'}
+      >
+        <Flex width="100%" justifyContent="right">
+          <Link href="/my-profile/edit">
+            <Icon
+              mt="15px"
+              aria-label="editar"
+              cursor="pointer"
+              fontSize="24px"
+              color="#281A45"
+              _hover={{ color: '#805AD5' }}
+            >
+              <MdEdit />
+            </Icon>
+          </Link>
+        </Flex>
+        <Box borderBottom="1px solid #B6B4BB">
+          <UserProfile
+            name={user.name}
+            username={user.username}
+            bio={user.bio}
+            insta={user.insta}
+            linkedin={user.linkedin}
+            github={user.github}
+            admin_user_block={false}
+          />
+        </Box>
+      </Flex>
     </>
   );
 }
