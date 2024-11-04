b473baf341d89d5b64019cb8b6600f1758e5490f
Clara Ferraz <claraferraz@proton.me>
2024-11-04 17:49:36 -0300

    Fix: loading no UsersPage e deletar UsersExamples

--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -37,7 +37,6 @@ export function UsersPage() {
 
   return (
     <>
-      {loading && <></>}
       <Tabs width="100%">
         <Tab width="100%" color="#281A45" cursor="zoom-in">
           Usuários
@@ -55,26 +54,30 @@ export function UsersPage() {
         <Input placeholder="Pesquisar usuário" />
       </InputGroup>
 
-      <Grid
-        justifyItems="center"
-        templateColumns={{
-          base: 'repeat(1, 1fr)',
-          sm: 'repeat(1, 1fr)',
-          md: 'repeat(2, 1fr)',
-          lg: 'repeat(2, 1fr)',
-        }}
-        gap="15px"
-        overflow="hidden"
-      >
-        {users.map((u: UserCardData) => (
-          <UserCard
-            name={u.name}
-            username={u.username}
-            postCount={u.postCount}
-            id={u.id}
-          />
-        ))}
-      </Grid>
+      {loading ? (
+        <p>carregando...</p>
+      ) : (
+        <Grid
+          justifyItems="center"
+          templateColumns={{
+            base: 'repeat(1, 1fr)',
+            sm: 'repeat(1, 1fr)',
+            md: 'repeat(2, 1fr)',
+            lg: 'repeat(2, 1fr)',
+          }}
+          gap="15px"
+          overflow="hidden"
+        >
+          {users.map((u: UserCardData) => (
+            <UserCard
+              name={u.name}
+              username={u.username}
+              postCount={u.postCount}
+              id={u.id}
+            />
+          ))}
+        </Grid>
+      )}
     </>
   );
 }
