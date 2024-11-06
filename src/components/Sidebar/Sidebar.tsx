e0b3e8ca8ae75d244c8c812156a9c7b771e0a004
Clara Ferraz <claraferraz@proton.me>
2024-11-06 17:33:10 -0300

    Styles: altura do sidebar e do register page

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -29,7 +29,7 @@ export default function SimpleSidebar({ children }: SidebarProps) {
 
   return (
     <Box
-    //se mexe aqui para tirar o bd gray.100
+      //se mexe aqui para tirar o bd gray.100
       minH="100vh"
       bg={useColorModeValue('gray.100', 'gray.900')}
       position="relative"
@@ -51,7 +51,7 @@ export default function SimpleSidebar({ children }: SidebarProps) {
 
       {!isDesktop && searchVisible && (
         <>
-          <Box px={4} zIndex={2} position="absolute" top={0} w={'full'}>
+          <Box px={4} zIndex={2} position="absolute" top="90px" w={'full'}>
             <InputGroup>
               <InputLeftElement children={<FiSearch color="#000" />} />
               <Input
