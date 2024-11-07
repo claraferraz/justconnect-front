3840efbb8f07b96122e561fb3d088564c116b839
Clara Ferraz <claraferraz@proton.me>
2024-11-07 12:11:56 -0300

    Feat: fetchPostsByUserId function

--- src/service/Post.ts
+++ src/service/Post.ts
@@ -1,18 +1,20 @@
-
-import { UserPostInfo, UserCreatePost } from "../interface/UserInterface";
-import api from "../service/api";
+import { UUID } from 'crypto';
+import { UserPostInfo, UserCreatePost } from '../interface/UserInterface';
+import api from '../service/api';
 
 const CreatePost = async (data: UserCreatePost) => {
-    const response = await api.post(`/posts`, data);
-    return response;
+  const response = await api.post(`/posts`, data);
+  return response;
 };
 
-
 const fetchPosts = async (): Promise<UserPostInfo[]> => {
+  const response = await api.get<UserPostInfo[]>('/public/posts');
+  return response.data;
+};
 
-      const response = await api.get<UserPostInfo[]>('/public/posts');
-      return response.data;
+const fetchPostsByUserId = async (id: UUID): Promise<UserPostInfo[]> => {
+  const response = await api.get<UserPostInfo[]>(`/public/posts/user/${id}`);
+  return response.data;
+};
 
-  };
-  
-export { CreatePost, fetchPosts }; 
+export { CreatePost, fetchPosts, fetchPostsByUserId };
