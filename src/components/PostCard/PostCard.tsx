3613c1068ba51bcf2baa1e9ed62672ee0d964ecf
Famgz <famgz@proton.me>
2024-11-13 15:49:18 -0300

    feat: display user posts in home page

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   Text,
   Card,
@@ -8,26 +9,31 @@ import {
   Tag,
 } from '@chakra-ui/react';
 
-import { FaUnlockAlt, FaLock } from "react-icons/fa";
+import { FaUnlockAlt, FaLock } from 'react-icons/fa';
 import { useEffect, useState } from 'react';
 import { formatDistanceToNow } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import { Link } from 'react-router-dom';
 import { UserPostInfo } from '../../interface/UserInterface';
 
+interface Props {
+  post: UserPostInfo;
+}
 
-export function PostCard({id, username, title, description, score, status_open, updated_at, tags ,created_at,commentCount }: UserPostInfo) {
-  
-  const parametros = {
-    id : id,
-    username:username, 
-    updated_at: updated_at, 
-    tags: tags,
-    created_at: created_at,
-    commentCount: commentCount
-  }
-  console.log(parametros)
-
+export function PostCard({
+  post: {
+    id,
+    username,
+    title,
+    description,
+    score,
+    status_open,
+    updated_at,
+    tags,
+    created_at,
+    commentCount,
+  },
+}: Props) {
   const [dateText, setDateText] = useState('');
 
   //atualiza automaticamente o tempo da postagem a cada 1 min
@@ -52,67 +58,79 @@ export function PostCard({id, username, title, description, score, status_open, 
             },
           }}
         >
-          <Flex justifyContent={'space-between'} alignItems={'center'} marginTop={"10px"} >
-            <Flex gap={"15px"} fontSize={"12px"} marginLeft={"20px"} color={"#515151"}>
-              <Text>
-                {score} curtidas
-              </Text>
-              <Text>
-                4 comentários
-              </Text>
+          <Flex
+            justifyContent={'space-between'}
+            alignItems={'center'}
+            marginTop={'10px'}
+          >
+            <Flex
+              gap={'15px'}
+              fontSize={'12px'}
+              marginLeft={'20px'}
+              color={'#515151'}
+            >
+              <Text>{score} curtidas</Text>
+              <Text>4 comentários</Text>
             </Flex>
-            {status_open ? (<FaUnlockAlt color={"#515151"} />) : (<FaLock  color={"#515151"} />)}
+            {status_open ? (
+              <FaUnlockAlt color={'#515151'} />
+            ) : (
+              <FaLock color={'#515151'} />
+            )}
           </Flex>
         </CardHeader>
         <CardBody>
-          <Link to={"/post/:id"}>
-            <Text fontSize={"16px"} bg="transparent" color={"#000000"} fontFamily="montserrat" fontWeight={"500"}>
-             {title}
+          <Link to={'/post/:id'}>
+            <Text
+              fontSize={'16px'}
+              bg="transparent"
+              color={'#000000'}
+              fontFamily="montserrat"
+              fontWeight={'500'}
+            >
+              {title}
             </Text>
           </Link>
-          <Text fontSize={"14px"} color={"#111111"} marginTop={"5px"}>
+          <Text fontSize={'14px'} color={'#111111'} marginTop={'5px'}>
             {description}
           </Text>
         </CardBody>
         <HStack spacing={4} width="100%" justifyContent="space-between">
           <Flex>
             {['md'].map((size) => (
-              <Link to={"/tags/:id"}>
+              <Link to={'/tags/:id'}>
                 <Tag
                   margin={'15px'}
                   size={size}
                   key={`${size}-1`}
                   variant="solid"
                   colorScheme="blue"
-                
                 >
                   +Tag
                 </Tag>
               </Link>
             ))}
             {['md'].map((size) => (
-              <Link to={"/tags/:id"}>
+              <Link to={'/tags/:id'}>
                 <Tag
                   margin={'15px'}
                   size={size}
                   key={`${size}-1`}
                   variant="solid"
                   colorScheme="yellow"
-                
                 >
                   +Tag
                 </Tag>
               </Link>
             ))}
             {['md'].map((size) => (
-              <Link to={"/tags/:id"}>
+              <Link to={'/tags/:id'}>
                 <Tag
                   margin={'15px'}
                   size={size}
                   key={`${size}-1`}
                   variant="solid"
                   colorScheme="green"
-                
                 >
                   +Tag
                 </Tag>
@@ -120,10 +138,10 @@ export function PostCard({id, username, title, description, score, status_open, 
             ))}
           </Flex>
           <Flex direction="column" alignItems="flex-end">
-            <Text fontSize="12px" fontFamily="montserrat" color={"#515151"}>
+            <Text fontSize="12px" fontFamily="montserrat" color={'#515151'}>
               {dateText}
             </Text>
-            <Link to={"/profile/:id"}>
+            <Link to={'/profile/:id'}>
               <Text fontSize="12px" fontFamily="montserrat" color="purple">
                 @username
               </Text>
