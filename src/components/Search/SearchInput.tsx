66c0e3a9cf525958ab995eb2c3affe9c9eae2175
Clara Ferraz <claraferraz@proton.me>
2024-11-18 19:46:02 -0300

    Feat: pesquisa funcionando, resultado no console.log

--- src/components/Search/SearchInput.tsx
+++ src/components/Search/SearchInput.tsx
@@ -9,12 +9,43 @@ import { InputGroup } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { SearchTypes } from '../../interface/SideBarInterface';
+import {
+  fetchSearchPosts,
+  fetchSearchComments,
+  fetchSearchUsers,
+  fetchSearchTags,
+} from '../../service/Search';
 
 export function SearchInput() {
   const [query, setQuery] = useState<string>('');
   const [debouncedQuery, setDebouncedQuery] = useState<string>('');
   const [type, setType] = useState<SearchTypes | string>(SearchTypes.posts);
   const isDesktop = useBreakpointValue({ base: false, md: true });
+
+  const handleSearch = async (type: SearchTypes | string, query: string) => {
+    let response;
+    try {
+      switch (type) {
+        case SearchTypes.posts:
+          response = await fetchSearchPosts(query);
+          return response.data;
+        case SearchTypes.comments:
+          response = await fetchSearchComments(query);
+          return response.data;
+        case SearchTypes.users:
+          response = await fetchSearchUsers(query);
+          return response.data;
+        case SearchTypes.tags:
+          response = await fetchSearchTags(query);
+          return response.data;
+
+        default:
+          break;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   useEffect(() => {
     const handler = setTimeout(() => {
@@ -30,6 +61,7 @@ export function SearchInput() {
     if (debouncedQuery) {
       console.log(debouncedQuery);
       console.log(type);
+      console.log(handleSearch(type, debouncedQuery));
     }
   }, [debouncedQuery, type]);
   return (
