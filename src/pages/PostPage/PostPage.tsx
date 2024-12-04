93219df534c89366a0cc303de275af47b37a0b75
Clara Ferraz <claraferraz@proton.me>
2024-12-04 14:18:42 -0300

    Stryles: ajuste alinhamento de menu e deleção de posts relacionados

--- src/pages/PostPage/PostPage.tsx
+++ src/pages/PostPage/PostPage.tsx
@@ -4,12 +4,11 @@ import {
   Box,
   Text,
   useBreakpointValue,
-  Tabs,
   Flex,
 } from '@chakra-ui/react';
 import { FiHeart } from 'react-icons/fi';
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import { DataText } from '../../components/DataText/DataText';
 import MenuPostComponent from '../../components/MenuPostComponent/MenuPostComponent';
@@ -21,11 +20,13 @@ import { useAuthStore } from '../../store/authStore';
 
 export function PostPage() {
   const { id } = useParams<{ id: string }>();
-  const userId = useAuthStore((state) => state.id); 
-  const { post, getPostById, incrementCommentCount, updatePostScore } = usePostStore();
-  const [liked, setLiked] = useState<boolean | null>(null); 
-  const [canComment, setCanComment] = useState<boolean>(true); 
+  const userId = useAuthStore((state) => state.id);
+  const { post, getPostById, incrementCommentCount, updatePostScore } =
+    usePostStore();
+  const [liked, setLiked] = useState<boolean | null>(null);
+  const [canComment, setCanComment] = useState<boolean>(true);
   const isDesktop = useBreakpointValue({ base: false, md: true });
+  const navigate = useNavigate();
 
   const getPost = async (postId: string) => {
     if (!postId) return;
@@ -38,40 +39,43 @@ export function PostPage() {
 
   const handleLike = async () => {
     if (!id || !userId) {
-      console.warn("Post ID ou User ID está ausente.");
+      console.warn('Post ID ou User ID está ausente.');
       return;
     }
-  
+
     try {
       if (liked) {
         const response = await removeUserPostLike(id, userId);
         if (response?.user_id === userId) {
           setLiked(false);
-          updatePostScore(id, -1); 
+          updatePostScore(id, -1);
         }
       } else {
-        const response = await createUserPostLike(id, userId); 
+        const response = await createUserPostLike(id, userId);
         if (response?.user_id === userId) {
           setLiked(true);
-          updatePostScore(id, 1); 
+          updatePostScore(id, 1);
         }
       }
-  
+
       await getPost(id);
     } catch (error) {
-      console.error("Erro ao curtir/descurtir o post:", error);
+      console.error('Erro ao curtir/descurtir o post:', error);
     }
   };
-  
+
   useEffect(() => {
     if (id) {
       getPost(id);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id]);
-  
+
   useEffect(() => {
     if (post) {
-      const userHasLiked = post.post_like.some((like) => like.user_id === userId);
+      const userHasLiked = post.post_like.some(
+        (like) => like.user_id === userId
+      );
       setLiked(userHasLiked);
       setCanComment(post.status_open);
     }
@@ -80,23 +84,46 @@ export function PostPage() {
   if (!post) return <Text>Post não encontrado</Text>;
 
   return (
-    <Box>
-      <Box mt="39px" display="flex" gap="35px">
-        <Text color="#805AD5" fontSize="14px" fontWeight="500" lineHeight="20px">
-          @{post.username}
-        </Text>
-        <Text
-          marginRight={isDesktop ? '350px' : '100px'}
-          fontSize="12px"
-          fontWeight="500"
-          color="#515151"
-        >
-          <DataText created={post.created_at} updated={post.updated_at} sufix />
-        </Text>
-        <MenuPostComponent setCanComment={setCanComment} canComment={canComment} />
+    <Box width={'100%'}>
+      <Box mt="39px" display="flex" justifyContent={'space-between'}>
+        <Flex gap="35px">
+          <Text
+            cursor={'pointer'}
+            onClick={() => navigate(`/profile/${post.username}`)}
+            _hover={{ color: '#281A45' }}
+            color="#805AD5"
+            fontSize="14px"
+            fontWeight="500"
+            lineHeight="20px"
+          >
+            @{post.username}
+          </Text>
+          <Text
+            marginRight={isDesktop ? '350px' : '100px'}
+            fontSize="12px"
+            fontWeight="500"
+            color="#515151"
+          >
+            <DataText
+              created={post.created_at}
+              updated={post.updated_at}
+              sufix
+            />
+          </Text>
+        </Flex>
+
+        <MenuPostComponent
+          setCanComment={setCanComment}
+          canComment={canComment}
+        />
       </Box>
 
-      <Text mt={isDesktop ? '24px' : '9px'} color="#000" fontSize="16px" fontWeight="600">
+      <Text
+        mt={isDesktop ? '24px' : '9px'}
+        color="#000"
+        fontSize="16px"
+        fontWeight="600"
+      >
         {post.title}
       </Text>
       <Box mt={isDesktop ? '24px' : '8px'} display="flex" alignItems="center">
@@ -107,7 +134,7 @@ export function PostPage() {
               width: '20px',
               height: '24px',
               cursor: 'pointer',
-              color: liked === null ? '#000' : liked ? '#805AD5' : '#000'
+              color: liked === null ? '#000' : liked ? '#805AD5' : '#000',
             }}
           />
           <Text fontSize="16px" fontWeight="600" color="#000">
@@ -129,15 +156,18 @@ export function PostPage() {
 
       <Box marginLeft={isDesktop ? '440px' : '126px'} mt="28px" display="flex">
         {post.tags.map((tag, index) => (
-          <Tag
-            key={index}
-            size="md"
-            variant="solid"
-            colorScheme="purple"
-            marginLeft={index > 0 ? '14px' : '0'}
-          >
-            {tag}
-          </Tag>
+          <Link to={`/tags/${tag}`}>
+            <Tag
+              key={index}
+              size="md"
+              variant="solid"
+              colorScheme="purple"
+              marginLeft={index > 0 ? '14px' : '0'}
+              _hover={{ background: '#815ad5d8' }}
+            >
+              {tag}
+            </Tag>
+          </Link>
         ))}
       </Box>
 
@@ -148,13 +178,16 @@ export function PostPage() {
             {post.score} curtida{post.score !== 1 ? 's' : ''}
           </Text>
           <Text mt="5px" color="#515151" fontSize="12px" fontWeight="500">
-            {post.comment.length} comentário{post.comment.length !== 1 ? 's' : ''}
+            {post.comment.length} comentário
+            {post.comment.length !== 1 ? 's' : ''}
           </Text>
         </Flex>
       </Box>
 
-      <CommentList comments={post.comment} refreshComments={() => id && getPost(id)} />
-
+      <CommentList
+        comments={post.comment}
+        refreshComments={() => id && getPost(id)}
+      />
       {canComment && (
         <CreateUserComment
           postId={id as string}
@@ -164,21 +197,6 @@ export function PostPage() {
       )}
 
       <Divider mt="40px" />
-
-      <Tabs
-        borderBottom="2px solid #281A45"
-        mt="16px"
-        variant="line"
-        display="flex"
-        width={isDesktop ? '655px' : '100%'}
-        height="54px"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Text color="#281A45" textAlign="center" fontSize="18px" fontWeight="500">
-          Relacionados
-        </Text>
-      </Tabs>
     </Box>
   );
 }
